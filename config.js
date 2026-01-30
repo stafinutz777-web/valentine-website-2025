d76acc9158a052145143ee90b815ecb137e69b49
stafinutz777-web <stafinutz777@gmail.com>
2026-01-30 05:13:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "inima mea",Bianca
+    valentineName: "inima mea",Bianca🩷
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
